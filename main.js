7426f7ac220eb045c66435726cc90af954be24b0
Tim Lee <TLee@Tims-MacBook-Air.local>
2016-10-07 15:24:44 -0700

    connected to ETSY api

---
+++ main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $.get("https://api.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop", function(data){
+       putInDom(data)
+       console.log(data)
+   })
+})
+
+
+function putInDom(data) {
+    var ourData = data.results.map(function(item){
+        return {
+            description: item.description,
+            price: item.price,
+            link: item.url,
+            title: item.title,
+            img: item.Images[0].url_570xN,
+            shopName: item.Shop.shop_name
+        }
+    })
+
+    var htmlStr = "";
+    ourData.forEach(function(item){
+        htmlStr += `
+        <div class="galleryItem">
+            <a href=${item.link}> 
+            <div><img src="${item.img}"/></div>
+            <p class="truncate"> ${item.title} </p>
+            <p> ${item.price} </p>
+            <p> ${item.shopName} </p>
+            </a>
+        </div>
+        `
+    })
+
+    $("#gallery").html(htmlStr)
+}
